5f893841792b3c672e301f86d7da44de34822fc3
Chris Young <cyoung43@byu.edu>
2021-11-20 09:07:32 -0800

    Initial Commit

---
+++ OpenSkyUtah/OpenSkyUtahApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenSkyUtahApp.swift
+//  OpenSkyUtah
+//
+//  Created by New User on 11/20/21.
+//
+
+import SwiftUI
+
+@main
+struct OpenSkyUtahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
