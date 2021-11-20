59836e866f512fccb93d26c860068465cc447d4d
Chris Young <cyoung43@byu.edu>
2021-11-20 10:02:55 -0800

    starting model

---
+++ OpenSkyUtah/OpenSkyStatus.swift
@@ -0,0 +1,15 @@
+//
+//  OpenSkyStatus.swift
+//  OpenSkyUtah
+//
+//  Created by Chris Young on 11/20/21.
+//
+
+import Foundation
+
+
+class OpenSkyService: ObservableObject {
+    @Published var aircraftStates: [AircraftState] = []
+    
+    
+}
